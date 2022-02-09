7be15face28c00bcbbc95d68da4f674d88bbfa04
HominSu <1774069959@qq.com>
2022-02-09 12:17:38 +0800

    fix get current user error

--- app/htpp/interface/internal/server/server.go
+++ app/htpp/interface/internal/server/server.go
@@ -1,11 +1,12 @@
 package server
 
 import (
-	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
 	"github.com/go-kratos/kratos/v2/registry"
 	"github.com/google/wire"
-	consulAPI "github.com/hashicorp/consul/api"
 	"htp-platform/app/htpp/interface/internal/conf"
+
+	consul "github.com/go-kratos/kratos/contrib/registry/consul/v2"
+	consulAPI "github.com/hashicorp/consul/api"
 )
 
 // ProviderSet is server providers.
